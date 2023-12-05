0d4760e14cc014181cc78fc152e1e11dd3f4971c
shaun <shaun@shaunwebber.uk>
2023-12-05 14:03:11 +0000

    rating update

--- checks/cloudtrail.py
+++ checks/cloudtrail.py
@@ -234,8 +234,8 @@ class cloudtrail(object):
             "analysis" : "",
             "description" : "AWS CloudTrail is a web service that records AWS API calls made in a given AWS account. The recorded information includes the identity of the API caller, the time of the API call, the source IP address of the API caller, the request parameters, and the response elements returned by the AWS service. CloudTrail uses Amazon S3 for log file storage and delivery, so log files are stored durably. In addition to capturing CloudTrail logs within a specified S3 bucket for long term analysis, realtime analysis can be performed by configuring CloudTrail to send logs to CloudWatch Logs. For a trail that is enabled in all regions in an account, CloudTrail sends log files from all those regions to a CloudWatch Logs log group. It is recommended that CloudTrail logs be sent to CloudWatch Logs. Note: The intent of this recommendation is to ensure AWS account activity is being captured, monitored, and appropriately alarmed on. CloudWatch Logs is a native way to accomplish this using AWS services but does not preclude the use of an alternate solution. Sending CloudTrail logs to CloudWatch Logs will facilitate real-time and historic activity logging based on user, API, resource, and IP address, and provides opportunity to establish alarms and notifications for anomalous or sensitivity account activity. ",
             "remediation" : "Ensure CloudTrail trails are integrated with CloudWatch Logs",
-            "impact" : "low",
-            "probability" : "low",
+            "impact" : "info",
+            "probability" : "info",
             "cvss_vector" : "N/A",
             "cvss_score" : "N/A",
             "pass_fail" : ""
